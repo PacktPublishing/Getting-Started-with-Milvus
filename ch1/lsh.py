f57a0c1e8bef94014fc02425b2af59f4710e591b
SimFG <1142838399@qq.com>
2025-07-25 20:20:05 +0800

    Update lsh.py to require numpy instead of gensim, and improve hash function to return bucket IDs. Refactor insert and query methods to use bucket IDs for better clarity and performance.

--- ch1/lsh.py
+++ ch1/lsh.py
@@ -1,5 +1,5 @@
-# Before running the script, you need to install the gensim python third-party library in advance.
-# Execute: pip install gensim
+# Before running the script, you need to install the numpy python third-party library in advance.
+# Execute: pip install numpy
 
 import numpy as np
 
@@ -8,6 +8,7 @@ class LSH:
         self.n_hashes = n_hashes
         self.n_buckets = n_buckets
         self.hash_tables = [{} for _ in range(n_hashes)]
+        np.random.seed(0)
         self.random_vectors = np.random.randn(n_hashes, 2)
 
     def _pairwise_distances(self, X, Y=None):
@@ -21,21 +22,24 @@ class LSH:
         return dist_matrix
 
     def _hash(self, point):
-        return tuple((np.dot(self.random_vectors, np.array(point)) > 0).astype(int))
+        bits = (np.dot(self.random_vectors, np.array(point)) > 0).astype(int)
+        return [int(''.join(map(str, bits[i:i+1])), 2) % self.n_buckets for i in range(self.n_hashes)]
 
     def insert(self, point):
+        bucket_ids = self._hash(point)
         for i in range(self.n_hashes):
-            hash_value = self._hash(point)
-            if hash_value not in self.hash_tables[i]:
-                self.hash_tables[i][hash_value] = []
-            self.hash_tables[i][hash_value].append(tuple(point))
+            bucket_id = bucket_ids[i]
+            if bucket_id not in self.hash_tables[i]:
+                self.hash_tables[i][bucket_id] = []
+            self.hash_tables[i][bucket_id].append(tuple(point))
 
     def query(self, query_point):
         candidates = set()
+        bucket_ids = self._hash(query_point)
         for i in range(self.n_hashes):
-            hash_value = self._hash(query_point)
-            if hash_value in self.hash_tables[i]:
-                candidates.update(self.hash_tables[i][hash_value])
+            bucket_id = bucket_ids[i]
+            if bucket_id in self.hash_tables[i]:
+                candidates.update(self.hash_tables[i][bucket_id])
 
         candidates = np.array([np.array(candidate) for candidate in candidates])
         if candidates.size == 0:
